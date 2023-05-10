35b8ebea3219c7b17c8959f5cb57f31e582b7d6f
Shritej-Reddy <tejureddy47@gmail.com>
2023-05-10 17:04:04 +0530

    reset everything

--- shritej-portfolio/src/App.tsx
+++ shritej-portfolio/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-        Hello World
+      Hello World
     </>
   )
 }
